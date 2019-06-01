e7266528960c9bd4c6384bc6c55bc69557011b9f
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2019-06-02 02:44:00 +0530

    Bind completed

--- Bind and this/index.js
+++ Bind and this/index.js
@@ -23,5 +23,44 @@ var sayHelloToKaren = sayHello.bind(person,"Namaste")(); //Here bind sets the va
 //Source material for this 
 /* 
     1) https://medium.com/@owenyangg/javascript-call-apply-and-bind-explained-to-a-total-noob-63f146684564
+    2) https://www.youtube.com/watch?v=g2WcckBB_q0
+    3) MPJ - Bind and this video
+
 
 */
+
+let c1 = {
+    x:5,
+    y:10
+};
+let c2= {
+    x:75,
+    y:235
+};
+
+function printCoordinates (){
+    console.log(this.x, this.y);
+}
+
+
+printCoordinates(); //this here is window object
+
+let c1_func = printCoordinates.bind(c1)();
+let c2_func = printCoordinates.bind(c2)();
+
+//more bind
+
+let crimeMasterGogo = {
+    sound : 'Awoo',
+    talk: function(){
+        console.log(this.sound);
+    }
+};
+
+crimeMasterGogo.talk(); //awoo
+
+// let talkFunction = crimeMasterGogo.talk; //ceases to be a method but is just a function. and this.sound in crimeMasterGogo is no longer crimeMasterGogo
+// talkFunction(); // will return undefined
+
+let boundFunction = crimeMasterGogo.talk.bind(crimeMasterGogo);
+boundFunction(); // will send out Awoo
